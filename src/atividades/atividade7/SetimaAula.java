f396f5d2a8ca94251092040da78f57ea599fd099
Ian <ianguilhermepenha@gmail.com>
2026-07-21 21:45:18 -0300

    Aula dia 21/07

---
+++ src/atividades/atividade7/SetimaAula.java
@@ -0,0 +1,15 @@
+package atividades.atividade7;
+
+/**
+ * ENCAPSULAMENTO
+ */
+public class SetimaAula {
+    /**
+     //modificadores de acesso
+
+     int idade;// DEFAULT
+     public String nome; // Pode ser acessada de qualquer lugar do meu projeto
+     private double altura; // Só é acessada dentro da sua classe Pai
+     protected String CPF; // Só pode ser acessado pela classe PAI e as classes FILHAS
+     */
+}
---
+++ src/aulas/heranca/locomocao/Veiculo.java
@@ -0,0 +1,16 @@
+package aulas.heranca.locomocao;
+
+/*
+* Herança
+* */
+
+public class Veiculo {
+    String marca;
+    String placa;
+    int anoFabricacao;
+    String nomeVeiculo;
+
+    public void ligar(){
+        System.out.println("O seu veículo " +nomeVeiculo+" está sendo ligado...");
+    }
+}
